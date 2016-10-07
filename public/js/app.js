d8616e4072e815077a64a33b235f11b5c9c50be1
Zeynep Uyanik <zeynep.uyanik@cs.com.tr>
2016-10-07 10:30:36 +0300

    added custom message event

--- public/js/app.js
+++ public/js/app.js
@@ -2,3 +2,8 @@ var socket = io();
 socket.on("connect", function(){
     console.log("connected to socket.io");
 });
+
+socket.on("message", function(message){
+    console.log(message.text);
+});
+
