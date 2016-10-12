85ebd6e940b80cd737e0d6d6725d73667932994d
Zeynep Uyanik <zeynep.uyanik@cs.com.tr>
2016-10-12 11:38:25 +0300

    is active/don't notify

--- public/js/app.js
+++ public/js/app.js
@@ -22,10 +22,11 @@ socket.on("message", function(message){
     var momentTimestamp  = moment.utc(message.timestamp);
     var $messages = jQuery(".messages");
     var $message = jQuery('<li class="list-group-item"></li>');
-    $("#title").text("Chat (New)");
-    if ($(document.activeElement).length > 0) {
-      $("#title").text("Chat");
+    if(message.name !== name){
+      $("#title").text("Chat (New)");
     }
+
+
     $message.append('<p> <strong>' + message.name + " " + momentTimestamp.local().format("HH:mma") +'</strong></p>');
     $message.append("<p>" + message.text +'</p>')
     $messages.append($message);
